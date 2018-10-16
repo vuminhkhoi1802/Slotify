2c4ecaa65846fb10264b5a79d31cf7c8587f1cf0
Khoi <vuminhkhoi1802@gmail.com>
2018-10-16 13:02:11 +0700

    s

--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -18,27 +18,6 @@ function sanitizeFormPassword($inputText){
     return $inputText;
 }
 
-function validateUsername($un){
-
-}
-
-function validateFirstname($fn){
-    
-}
-
-function validateLastname($ln){
-    
-}
-
-function validateEmails($em, $em2){
-    
-}
-
-function validatePasswords($pw, $pw2){
-    
-}
-
-
 if(isset($_POST['registerButton'])) {
     //Register button was pressed
 
@@ -50,13 +29,10 @@ if(isset($_POST['registerButton'])) {
     $password = sanitizeFormPassword($_POST['password']);
     $password2 = sanitizeFormPassword($_POST['password2']);
 
-    validateUsername($username);
-    validateFirstname($firstName);
-    validateLastname($lastName);
-    validateEmails($emailID, $emailConfirmation);
-    validatePasswords($password, $password2);
-
+    $account = new Account();
 
 
 }
+
+
 ?>
